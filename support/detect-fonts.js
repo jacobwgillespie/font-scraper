e0b206cc125a1b716aa989434b306c7e00ea04f3
Jacob Gillespie <jacobwgillespie@gmail.com>
2016-10-20 11:56:14 -0500

    Remove old debug comments

--- support/detect-fonts.js
+++ support/detect-fonts.js
@@ -3,11 +3,8 @@ function detectFonts() {
 
   function walk(node) {
     if (node.nodeType == 1) {
-      // console.log(node.tagName);
-
       try {
         var f = document.defaultView.getComputedStyle(node, null).getPropertyValue('font-family');
-        // console.log(node, f);
         fonts[f] = true;
       } catch(e) {
         // ignore this for now
